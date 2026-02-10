1a84ddd352b7968e9e992136d2e80e90e4a3d2d7
Sonya3008 <113394508+Sonya3008@users.noreply.github.com>
2026-02-10 13:41:51 +0300

    Update sql_checks.sql

--- 4_sql/sql_checks.sql
+++ 4_sql/sql_checks.sql
@@ -1,16 +1,16 @@
 Примеры запросов:
 
-Проверка сохранения события:
+- Проверка сохранения события:
   
-SELECT * FROM events WHERE camera_id = 1 ORDER BY created_at DESC;
+  SELECT * FROM events WHERE camera_id = 1 ORDER BY created_at DESC;
 
-Проверка отсутствия дублирования событий:
+- Проверка отсутствия дублирования событий:
   
-SELECT camera_id, timestamp, COUNT(*)
-FROM events
-GROUP BY camera_id, timestamp
-HAVING COUNT(*) > 1;
+  SELECT camera_id, timestamp, COUNT(*)
+  FROM events
+  GROUP BY camera_id, timestamp
+  HAVING COUNT(*) > 1;
 
-Проверка архива:
+- Проверка архива:
 
-SELECT * FROM video_archive WHERE camera_id = 1;
+  SELECT * FROM video_archive WHERE camera_id = 1;
